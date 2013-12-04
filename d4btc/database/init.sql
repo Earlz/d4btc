a203837da14f3763a5a2696a5ba150893e702702
Jordan Earls <earlz@earlz.net>
2013-12-04 01:28:09 -0500

    Initial go at the data structure

---
+++ d4btc/database/init.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE Config
+(
+   rpcuser varchar(512),
+   rpcpassword varchar(512),
+   rpcurl varchar(512),
+   confirmations int,
+   
+);
+
+create table Products
+(
+    id serial primary key,
+    orderid int unique,
+    name varchar(64),
+    description text,
+    min_price decimal(8,8) not null,
+    max_price decimal(8,8),
+    download varbit,
+    callback_url varchar
+);
+
