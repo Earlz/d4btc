e9e5ddc4d04aea95cea5f94ea637440b87c3ad53
Jordan Earls <earlz@earlz.net>
2013-12-04 01:47:59 -0500

    more crap

--- d4btc/database/init.sql
+++ d4btc/database/init.sql
@@ -4,9 +4,9 @@
    rpcpassword varchar(512),
    rpcurl varchar(512),
    confirmations int,
-   
+   redownload_window int, --an amount in seconds
 );
-
+--todo:defaults
 create table Products
 (
     id serial primary key,
@@ -16,6 +16,21 @@ create table Products
     min_price decimal(8,8) not null,
     max_price decimal(8,8),
     download varbit,
-    callback_url varchar
+    callback_url varchar,
+    refundable boolean
 );
 
+create table Transactions
+(
+    id serial primary key,
+    product_id int not null,
+    to_address varchar(50) not null,
+    from_address varchar(50),
+    pending boolean not null,
+    refunded boolean not null,
+    available_from timestamp,
+    downloaded_at timestamp,
+    secret_id uuid not null,
+    
+    
+);
