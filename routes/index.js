9f20fcee94148744139a0f5ecb428ca439bc826f
Jordan Earls <earlz@earlz.net>
2013-12-10 01:04:04 -0500

    Change async structure so that bitcoinrpc and postgresql are called in parallel Add async package cause no way I will be writing my own

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,31 @@
 //get index
 require('./../config');
+async=require('async');
 var bitcoin=require('bitcoin').bitcoin;
 var knex=require('knex').knex;
 
 //var bookshelf=require('bookshelf').bookshelf;
 
 exports.index = function(req, res){
-  var testnet='';
-  bitcoin.getInfo(function(e, info){
-    if(e){ return console.log(e);}
-    if(info.testnet){
-      testnet='bitcoin RPC is testnet';
-    }else{
-      testnet='nope.. you must be crazy';
+  testnet: async.parallel({
+    testnet: function(callback){
+      bitcoin.getInfo(function(e, info){
+        if(e){ return console.log(e);}
+        if(info.testnet){
+          callback(null, 'bitcoin RPC is testnet');
+        }else{
+          callback(null, 'nope.. you must be crazy');
+        }
+      });
+    },
+    name: function(callback){
+      var c=knex('config').select().then(function(r){
+        callback(null, r[0].site_name);
+      });
     }
-    var c=knex('config').select().then(function(k){
-      res.render('index', { title: k[0].site_name, testnet: testnet });
-    });
+  },
+  function(err, r){
+      res.render('index', { title: r.name, testnet: r.testnet });
   });
-  //var title=
+  
 };
